18287ecffdff9baa3d1aa0b985a69a5e10b75df0
keerthi0667-blip <keerthi06.6.7@gmail.com>
2025-08-31 14:18:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
+
+document.querySelector("form").
+addEventListener("submit", function(e)
+ {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+  this.reset();
+});
